10e6c2b805855fc46589c47f05d6fa0434d26e57
mazi-kunle <olakunlemichael644@gmail.com>
2022-08-31 08:19:17 -0700

    finished task 3

--- models/base_model.py
+++ models/base_model.py
@@ -9,13 +9,22 @@ class BaseModel:
     '''
     A class that defines all common attributes/methods for other classes.
     '''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''
         An init function.
         '''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(kwargs[key])
+                if key == '__class__':
+                    pass
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         '''
