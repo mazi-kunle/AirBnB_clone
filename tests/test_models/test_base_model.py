17cffc03f26ddb39f99b3149b66b710570a057a7
Ch@rlem@gne <cagbolossou@dactechnologies.net>
2022-09-06 12:21:08 +0000

    tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,8 +2,10 @@
 """
 Test BaseModel class module
 """
+from statistics import mode
 import unittest
 from models.base_model import BaseModel
+from models import storage
 from datetime import datetime
 
 
@@ -16,11 +18,26 @@ class TestBaseModel(unittest.TestCase):
         """
         Test attributes exists
         """
+        model = BaseModel()
+        self.assertTrue(type(model.id), str)
+        self.assertTrue(type(model.created_at), datetime)
+        self.assertTrue(type(model.updated_at), datetime)
+        self.assertEqual(model.updated_at.year, model.created_at.year)
+        self.assertEqual(model.updated_at.month, model.created_at.month)
+        self.assertEqual(model.updated_at.day, model.created_at.day)
+        self.assertEqual(model.updated_at.hour, model.created_at.hour)
+
+    def test_str_representaion(self):
+        """string representaion test.
+        """
         my_model = BaseModel()
-        self.assertTrue(type(my_model.id), str)
-        self.assertTrue(type(my_model.created_at), datetime)
-        self.assertTrue(type(my_model.updated_at), datetime)
-        self.assertEqual(my_model.updated_at.year, my_model.created_at.year)
-        self.assertEqual(my_model.updated_at.month, my_model.created_at.month)
-        self.assertEqual(my_model.updated_at.day, my_model.created_at.day)
-        self.assertEqual(my_model.updated_at.hour, my_model.created_at.hour)
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+        self.assertEqual(
+            str(my_model),
+            '[{}] ({}) {}'.format(
+                my_model.__class__.__name__,
+                my_model.id,
+                my_model.__dict__
+            )
+        )
